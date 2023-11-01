9a3e6fa6d1714ccdfaafe59e6a719fd9e041d637
JaretLynch <90110874+JaretLynch@users.noreply.github.com>
2023-11-01 19:06:29 -0400

    Update Server.c

--- Server.c
+++ Server.c
@@ -954,7 +954,7 @@ void server_loop() {
 
 								  		if (strcmp(Arg1,ClientIP)==0 && currentClient.FD!=sock_index){
 
-								  			printf("Client exists!");
+								  			printf("Client exists!"\n);
 
 								  			fflush(stdout);
 
