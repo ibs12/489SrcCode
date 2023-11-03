33d4e81ce4c132388fa0be7a856d5bfbafa244b2
JaretLynch <90110874+JaretLynch@users.noreply.github.com>
2023-11-03 09:58:01 -0400

    Update Server.c

--- Server.c
+++ Server.c
@@ -596,19 +596,19 @@ int initialize_server(int port) {
 
 /*Backlog EmptyLog;*/
 
-EmptyLog = (Backlog*)malloc(sizeof(Backlog));
+EmptyLog* = (Backlog*)malloc(sizeof(Backlog));
 
 
 
 /*Message Empty;*/
 
-Empty = (Message*)malloc(sizeof(Message));
+Empty* = (Message*)malloc(sizeof(Message));
 
 
 
 /*Client Dummy;*/
 
-	Dummy = (Client*)malloc(sizeof(Client));
+	Dummy* = (Client*)malloc(sizeof(Client));
 
 	Dummy.FD=-1;
 
