a958c6fde672db00a4c36fee5819b0bb349b9659
JaretLynch <90110874+JaretLynch@users.noreply.github.com>
2023-11-05 04:03:45 -0500

    Update Client.c

--- Client.c
+++ Client.c
@@ -552,6 +552,16 @@ void process_client_commands() {
 
 							}
 
+							else if (strcmp(Command,"UNBLOCK")==0){
+
+								int j=send(ClientFD,Input,strlen(Input),0);
+
+
+
+								free(Input);
+
+							}
+
 							else{
 
 
